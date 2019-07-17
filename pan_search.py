796610a555043e594b46fa2670fcc4438ffb74ab
gthber <panda1987cs@gmail.com>
2019-07-17 16:11:28 +0800

    Update pan_search.py

--- pan_search.py
+++ pan_search.py
@@ -29,7 +29,7 @@ while True:
 	try:
 	    for num in range(1,2):
 		#site = "http://www.panduoduo.net/s/comb/n-"+urllib2.quote(pankey)+"&s-size1&f-f4/"+str(num)
-		site = "http://www.panduoduo.net/s/comb/n-"+str(pankey)+"&s-vcnt1&ty-bd&nd-1/"+str(num)
+		site = "http://www.panduoduo.net/s/comb/n-"+str(pankey)+"&s-vcnt1&ty-bd/"+str(num)
 		hdr = {'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.64 Safari/537.11',
 		       'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
 		       'Accept-Charset': 'ISO-8859-1,utf-8;q=0.7,*;q=0.3',
